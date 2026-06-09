25b65615aa0702f5365a8f1bb6a5a7c34c4a90ff
DibyanshuSah <dibyanshusah2004@gmail.com>
2026-06-09 18:21:34 +0530

    code update

--- cmd/main.go
+++ cmd/main.go
@@ -11,32 +11,32 @@ import (
 
 func main() {
 	pdfPath := "uploads/demo.PDF"
-	// Step 1: Validate the given PDF
+	// 1st: Validate the given PDF
 	err := utils.ValidatePDF(pdfPath)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	// Step 2: extraction of the raw text from the PDF
+	// 2nd: extraction of the raw text from the PDF
 	rawText, err := pdf.ExtractText(pdfPath)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	// Step 3: Saving of the extracted raw text
+	// 3rd: Saving of the extracted raw text
 	err = utils.SaveText("output/raw.txt", rawText)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	// Step 4: parsing the names if the medicine
+	// 4th: parsing the names if the medicine
 	medicines := parser.ParseMedicines(rawText)
 	err = utils.SaveJSON("output/medicines.json", medicines)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	// Step 5: removing the exact duplicates from the log
+	// 5th: removing the exact duplicates from the log
 	uniqueMedicines := normalizer.BuildUniqueCatalog(medicines)
 
 	err = utils.SaveJSON("output/unique_medicines.json", uniqueMedicines)
@@ -45,7 +45,7 @@ func main() {
 		return
 	}
 
-	// Step 6: generating the drug catalogs 
+	// 6th: generating the drug catalogs 
 	drugCatalog := catalog.BuildDrugCatalog(uniqueMedicines)
 
 	err = utils.SaveJSON("output/drug_catalog.json", drugCatalog)
@@ -68,7 +68,7 @@ func main() {
 	for i := 0; i < limit; i++ {
 		fmt.Printf("%d. %s\n", i+1, drugCatalog[i].DrugName)
 	}
-	
+
 	fmt.Println()
 	fmt.Println("raw.txt saved successfully")
 	fmt.Println("medicines.json saved successfully")
