f1a0d376661e76b55698147f79020fbe574c0bc6
DibyanshuSah <dibyanshusah2004@gmail.com>
2026-06-09 17:55:30 +0530

    updated code

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-
 	"stockist-parser/internal/catalog"
 	"stockist-parser/internal/normalizer"
 	"stockist-parser/internal/parser"
@@ -11,40 +10,33 @@ import (
 )
 
 func main() {
-
 	pdfPath := "uploads/demo.PDF"
-
-	// Step 1: Validate PDF
+	// Step 1: Validate the given PDF
 	err := utils.ValidatePDF(pdfPath)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
-	// Step 2: Extract raw text from PDF
+	// Step 2: extraction of the raw text from the PDF
 	rawText, err := pdf.ExtractText(pdfPath)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
-	// Step 3: Save extracted raw text
+	// Step 3: Saving of the extracted raw text
 	err = utils.SaveText("output/raw.txt", rawText)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
-	// Step 4: Parse medicine names
+	// Step 4: parsing the names if the medicine
 	medicines := parser.ParseMedicines(rawText)
-
 	err = utils.SaveJSON("output/medicines.json", medicines)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
-	// Step 5: Remove exact duplicates
+	// Step 5: removing the exact duplicates from the log
 	uniqueMedicines := normalizer.BuildUniqueCatalog(medicines)
 
 	err = utils.SaveJSON("output/unique_medicines.json", uniqueMedicines)
@@ -53,7 +45,7 @@ func main() {
 		return
 	}
 
-	// Step 6: Generate drug catalog
+	// Step 6: generating the drug catalogs 
 	drugCatalog := catalog.BuildDrugCatalog(uniqueMedicines)
 
 	err = utils.SaveJSON("output/drug_catalog.json", drugCatalog)
@@ -62,7 +54,6 @@ func main() {
 		return
 	}
 
-	// Summary
 	fmt.Println()
 	fmt.Println("Total Medicines :", len(medicines))
 	fmt.Println("Unique Medicines:", len(uniqueMedicines))
@@ -74,11 +65,10 @@ func main() {
 	if len(drugCatalog) < limit {
 		limit = len(drugCatalog)
 	}
-
 	for i := 0; i < limit; i++ {
 		fmt.Printf("%d. %s\n", i+1, drugCatalog[i].DrugName)
 	}
-
+	
 	fmt.Println()
 	fmt.Println("raw.txt saved successfully")
 	fmt.Println("medicines.json saved successfully")
